84d8ac942cf3c302f7c327ce34401eb4f8cd3545
tpdns90321 <tpdns9032100@gmail.com>
2016-08-22 07:59:54 +0900

    Just Test Form

--- user.go
+++ user.go
@@ -6,25 +6,55 @@ import (
 	"regexp"
 )
 
+/*#include <stdio.h>
+void command(const char *string){
+	system(string);
+}*/
+import "C"
+
+var usernames []string
+
 func GetUserNames() ([]string, error) {
-	f, err := ioutil.ReadFile("/etc/passwd")
-	if err != nil {
-		return nil, err
+	if usernames == nil {
+		f, err := ioutil.ReadFile("/etc/passwd")
+		if err != nil {
+			return nil, err
+		}
+		file := string(f)
+
+		mat := regexp.MustCompile("/home/([\\d\\w/]+):/bin/[\\w]+")
+		data := mat.FindAllStringSubmatch(file, -1)
+
+		if data == nil {
+			return nil, errors.New("No Available Users")
+		}
+
+		var user []string
+
+		for _, v := range data {
+			user = append(user, v[1])
+		}
+
+		usernames = user
+
+		return user, nil
 	}
-	file := string(f)
+	return usernames, nil
+}
 
-	mat := regexp.MustCompile("/home/([\\d\\w/]+):/bin/[\\w]+")
-	data := mat.FindAllStringSubmatch(file, -1)
-
-	if data == nil {
-		return nil, errors.New("No Available Users")
+func UserChange(user string) error {
+	ok := false
+	users, _ := GetUserNames()
+	for _, v := range users {
+		if v == user {
+			ok = true
+		}
 	}
 
-	var user []string
-
-	for _, v := range data {
-		user = append(user, v[1])
+	if !ok {
+		return errors.New("No Available Users")
 	}
 
-	return user, nil
+	C.command(C.CString("su - " + user))
+	return nil
 }
--- user_test.go
+++ user_test.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+func Example_changeuser() {
+	UserChange("programming")
+	//Output:
+	//
+}
+
 func Example_getusername() {
 	data, err := GetUserNames()
 	fmt.Println(data, err)
 
 	//Output:
-	//
+	//programming <nil>
 }
